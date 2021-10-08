d82941d17bb0722c78ce3813baa25e00bfaee465
MohammadRimawi <mohrimawiz@gmail.com>
2021-10-08 23:53:31 +0300

    adding scss

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask,flash, render_template, request, session, jsonify,url_for,redirect,make_response
 from flask_assets import Environment, Bundle
+from flask_scss import Scss
+
 
 app = Flask(__name__)
+Scss(app)
+
+
+
+bundles = {
+    "flex.css": Bundle('css/flex.css', output = "gen/flex.css"),
+    "general.css": Bundle('assets/scss/general.scss', filters='pyscss', output='gen/general.css')
+}
+
+assets = Environment(app)
+assets.register(bundles)
+
 
 
 @app.route('/')

ddab60988d95da1cd0db8996e9853dfd66fdcf3d
root <root@_HOSTNAME_>
2021-10-18 22:49:48 +0200

    adding wsgi

---
+++ app.wsgi
@@ -0,0 +1,5 @@
+import sys
+
+sys.path.insert(0, "/var/www/html/Rimawi-site")
+
+from init import app as application
--- init.py
+++ init.py
@@ -508,8 +508,8 @@ def index():
 
 
 
-if __name__ == '__main__':
-    app.run(host = '0.0.0.0',debug=True)
+#if __name__ == '__main__':
+#    app.run(host = '0.0.0.0',debug=True)
 
 
     
